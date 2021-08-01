071982c293ad3232ca640db31c8bbf3114015790
kamal chamkh <kamal.chamkh@adesso.de>
2021-08-01 23:30:53 +0200

    6

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker Worlddd yooo";
+		return "<h1>Hello Docker World</h1>";
 	}
 
 	public static void main(String[] args) {
