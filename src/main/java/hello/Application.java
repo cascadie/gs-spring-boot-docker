878eea1e0604c84f2f8b65ff5bd4ec41a0e7ee88
cascadie <cascadie@hotmail.de>
2021-08-01 23:26:58 +0200

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker Worlddd";
+		return "Hello Docker Worlddd yooo";
 	}
 
 	public static void main(String[] args) {
