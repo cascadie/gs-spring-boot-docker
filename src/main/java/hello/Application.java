3c937d3aac849c06d8f8376789cb532a00231c09
kamal chamkh <kamal.chamkh@adesso.de>
2021-08-07 22:56:46 +0200

    6

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "<h1>Hello Marita</h1>";
+		return "<h1>Hello World</h1>";
 	}
 
 	public static void main(String[] args) {
