83c5fe503a2289522ded0b8159f36375af6b0ce1
cascadie <cascadie@hotmail.de>
2021-08-16 04:21:23 +0200

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,29 +11,12 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		String a="a";
-		a="a";
-		a="a";
-		String b="a";
-		b="a";
-		b="a";
-		a=b;
-		int x=0;
-for (int i = 0; i < 10; i++) { // noncompliant, loop only executes once
-  break;
-}
-		for (int i = 0; i < 10; i++) { // noncompliant, loop only executes once
-  break;
-}
 		return "<h1>Hello World</h1>";
 		
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		for (int i = 0; i < 10; i++) { // noncompliant, loop only executes once
-  break;
-}
 	}
 
 }
