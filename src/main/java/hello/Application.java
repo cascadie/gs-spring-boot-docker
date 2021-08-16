d39e7c90797a8d1f667afb5e9918f6b782c78043
cascadie <cascadie@hotmail.de>
2021-08-16 03:36:58 +0200

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -18,11 +18,36 @@ public class Application {
 		b="a";
 		b="a";
 		a=b;
+		for (int i = 0; i < 10; i++) { // noncompliant, loop only executes once
+  if (i == x) {
+    break;
+  } else {
+    printf("i is %d", i);
+    return;
+  }
+}
+		for (int i = 0; i < 10; i++) { // noncompliant, loop only executes once
+  if (i == x) {
+    break;
+  } else {
+    printf("i is %d", i);
+    return;
+  }
+}
 		return "<h1>Hello World</h1>";
+		
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		for (int i = 0; i < 10; i++) { // noncompliant, loop only executes once
+  if (i == x) {
+    break;
+  } else {
+    printf("i is %d", i);
+    return;
+  }
+}
 	}
 
 }
